a66fe8681818e038e57ee5a95b7051b0eccb2d26
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-07-17 19:22:00 -0400

    syntax error in alter column

--- db/migrate/20150717231551_add_new_group_column.rb
+++ db/migrate/20150717231551_add_new_group_column.rb
@@ -1,8 +1,8 @@
 class AddNewGroupColumn < ActiveRecord::Migration
   def change
   	execute '
-      ALTER TABLE "users"
-      ADD COLUMN "user_group"
-      TYPE integer'
+      ALTER TABLE users
+      ADD COLUMN user_group
+      TYPE integer;'
   end
 end
