d990b99fefe561746be5d1f46b22520d443ba289
Brad Hudson <bradley.hudson@careerbuilder.com>
2015-07-17 19:10:46 -0400

    adding group column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717144129) do
+ActiveRecord::Schema.define(version: 20150717225648) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "poll"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
